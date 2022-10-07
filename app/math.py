2f455a4c37a1b4d44caf681b772abcdafcb37d9c
Justin Chiou <chiou.kai@gmail.com>
2022-10-07 16:05:41 -0400

    fix: lint

--- app/math.py
+++ app/math.py
@@ -3,6 +3,7 @@ from typing import Union
 
 
 def multiply_two_numbers(
-    a: Union[int, Real], b: Union[int, Real]
+    a: Union[int, Real],
+    b: Union[int, Real],
 ) -> Union[int, Real]:
     return a * b
